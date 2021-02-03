811c9823359d9770b25735af386d085dacdd84d4
Sebastian F <sebpfave2@gmail.com>
2021-02-02 21:35:28 -0500

    Using lodash for post route checking

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const lodash = require("lodash");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -50,21 +51,18 @@ app.post("/compose", (req, res) => {
 });
 
 app.get("/posts/:postTitle", (req, res) => {
-  const requestedPost = req.params.postTitle;
+  const requestedPost = lodash.lowerCase(req.params.postTitle);
 
   blogPosts.forEach(post => {
-    const postTitle = post.title;
+    const postTitle = lodash.lowerCase(post.title);
 
     if (postTitle === requestedPost) {
       console.log("Match found");
+    } else {
+      console.log("No match");
     }
   });
 });
-
-
-
-
-
 
 
 app.listen(3000, function () {
